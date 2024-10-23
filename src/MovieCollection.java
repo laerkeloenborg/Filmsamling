42e94815ba2a924add2f48ef1eaaed1b5e830dfb
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2024-10-23 09:28:08 +0200

    MovieCollection

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class MovieCollection {
+    private ArrayList<Movie> movieCollection = new ArrayList<>();
+
+    public MovieCollection(ArrayList<Movie> movieCollection){
+        this.movieCollection = movieCollection;
+    }
+
+    public ArrayList<Movie> getMovieCollection(){
+        return movieCollection;
+    }
+
+    public void setMovieCollection(ArrayList<Movie> movieCollection){
+        this.movieCollection = movieCollection;
+    }
+
+    public void addMovie(Movie movie){
+        movieCollection.add(movie);
+    }
+
+
+
 }
