e15a8def7e0bcbe8c0858348f63b346f2394908f
KasperPersson <kape0010@stud.kea.dk>
2024-10-23 11:45:33 +0200

    Search-funktion

--- src/Controller.java
+++ src/Controller.java
@@ -4,10 +4,12 @@ public class Controller {
     private Movie movie;
     private MovieCollection movieCollection1 = new MovieCollection();
     Scanner input = new Scanner(System.in);
+    Movie spiderman = new Movie("Spiderman", "P", 1234, true, 120, "action");
 
 
 
     public void startCollection() {
+        movieCollection1.addMovie(spiderman);
         System.out.println("welcome to my movie collection!");
         int addingmovies = -1;
         while (addingmovies != 4) {
@@ -15,7 +17,8 @@ public class Controller {
                     "\npress:\n" +
                             "1. for entering a film to the collection\n" +
                             "2. for viewing of movie collection\n" +
-                            "3. for exiting");
+                            "3. for searching movie\n" +
+                            "4. for exiting");
             addingmovies = input.nextInt();
             switch (addingmovies) {
                 case 1:
@@ -48,11 +51,15 @@ public class Controller {
                     System.out.println(movieCollection1.toString());
                     break;
                 case 3:
+                    System.out.println("Enter the title of your movie");
+                    String searchTitle = input.next();
+                    String searchResult = movieCollection1.searchMovie(searchTitle);
+                    System.out.println(searchResult);
+                case 4:
                     System.out.println("you are now exiting the movie collection");
                     break;
                 default:
             }
         }
     }
-
 }
