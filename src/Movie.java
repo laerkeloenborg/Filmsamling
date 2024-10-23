daa8faaf372735c1c52038eb087900f3ab1b57f4
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2024-10-23 09:16:43 +0200

    movie class

--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,69 @@
 public class Movie {
+    private String title;
+    private String director;
+    private int yearCreated;
+    private boolean isInColor;
+    private double lengthInMinutes;
+    private String genre;
+
+    public Movie (String title, String director, int yearCreated, boolean isInColor, double lengthInMinutes, String genre){
+        this.title = title;
+        this.director = director;
+        this.yearCreated = yearCreated;
+        this.isInColor = isInColor;
+        this.lengthInMinutes = lengthInMinutes;
+        this.genre = genre;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle (String title){
+        this.title = title;
+    }
+
+    public String getDirector(){
+        return director;
+    }
+
+    public void setDirector(String director){
+        this.director = director;
+    }
+
+    public int getYearCreated(){
+        return yearCreated;
+    }
+
+    public void setYearCreated(int yearCreated){
+        this.yearCreated = yearCreated;
+    }
+
+    public boolean getIsInColor(){
+        return isInColor;
+    }
+
+    public void setIsInColor(boolean isInColor){
+        this.isInColor = isInColor;
+    }
+
+    public double getLengthInMinutes(){
+        return lengthInMinutes;
+    }
+
+    public void setLengthInMinutes(double lengthInMinutes){
+        this.lengthInMinutes = lengthInMinutes;
+    }
+
+    public String getGenre(){
+        return genre;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+
+
+
 }
