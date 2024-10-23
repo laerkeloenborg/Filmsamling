db4199b121d379a20d84bdb66ee84fae851dd618
christianvinther <christian@vinther.dk>
2024-10-23 09:30:09 +0200

    Commiting

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Movie movie = new Movie("SpiderMan", "Jon Watts", 2021, true, 120, "action" );
+        Scanner addMovie = new Scanner(System.in);
+
+        System.out.println(movie);
+
 
 
 
 
     }
+
 }
