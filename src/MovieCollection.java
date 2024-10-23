426f1b18f50e9ccf83c72dcb3702ff84091a42c5
christianvinther <christian@vinther.dk>
2024-10-23 10:55:36 +0200

    searchMovie

--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -3,22 +3,30 @@ import java.util.ArrayList;
 public class MovieCollection {
     private ArrayList<Movie> movieCollection = new ArrayList<>();
 
-    public MovieCollection(ArrayList<Movie> movieCollection){
+    public MovieCollection(ArrayList<Movie> movieCollection) {
         this.movieCollection = movieCollection;
     }
 
-    public ArrayList<Movie> getMovieCollection(){
+    public ArrayList<Movie> getMovieCollection() {
         return movieCollection;
     }
 
-    public void setMovieCollection(ArrayList<Movie> movieCollection){
+    public void setMovieCollection(ArrayList<Movie> movieCollection) {
         this.movieCollection = movieCollection;
     }
 
-    public void addMovie(Movie movie){
+    public void addMovie(Movie movie) {
         movieCollection.add(movie);
     }
 
+    public Movie searchMovie(String title) {
+        String noMovies = "There is no movies on your list with the title";
+        for (Movie movieCollection : movieCollection) {
+            if (movieCollection.getTitle().equalsIgnoreCase(title)) {
+                return movieCollection;
+            }
+        }
 
-
+        return null;
+    }
 }
