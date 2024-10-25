408c212f36595051be8424ce479aeafb8298d7f5
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2024-10-25 11:21:13 +0200

    test

--- test/MovieCollectionTest.java
+++ test/MovieCollectionTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,12 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MovieCollectionTest {
 
+    @DisplayName("search for 3 movies")
     @Test
-    void getMovieCollection() {
+    void getMovieCollection3() {
         //Arrange
         Movie movie = new Movie("Spiderman", "Dorte", 2024, true, 110,"action");
         Movie movie2 = new Movie("The holiday", "Tom", 2020, false, 1000, "RomCom");
-        Movie movie3 = new Movie("it", "bob", 2930, true, 192, "horror");
+        Movie movie3 = new Movie("it", "bob", 2019, true, 192, "horror");
         MovieCollection movieCollection = new MovieCollection();
 
         movieCollection.addMovie(movie);
@@ -24,6 +26,40 @@ class MovieCollectionTest {
 
         //Assert
         int expectedResult = 3;
+        assertEquals(expectedResult,actualSize);
+
+    }
+
+    @DisplayName("search for 1 movie")
+    @Test
+    void getMovieCollection1() {
+        //Arrange
+        Movie movie = new Movie("Spiderman", "Dorte", 2024, true, 110,"action");
+        MovieCollection movieCollection = new MovieCollection();
+
+        movieCollection.addMovie(movie);
+
+        //Act
+        int actualSize = movieCollection.getNumberOfMovies();
+
+        //Assert
+        int expectedResult = 1;
+        assertEquals(expectedResult,actualSize);
+
+    }
+
+    @DisplayName("search for 0 movies")
+    @Test
+    void getMovieCollection0() {
+        //Arrange
+        MovieCollection movieCollection = new MovieCollection();
+
+
+        //Act
+        int actualSize = movieCollection.getNumberOfMovies();
+
+        //Assert
+        int expectedResult = 0;
         assertEquals(expectedResult,actualSize);
 
     }
