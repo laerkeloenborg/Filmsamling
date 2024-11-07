6379d7f37c32a24752febe52e3b648cb25fa793f
lalo0001@stud.kea.dk <laerkeloenborg@MacBook-Air-tilhrende-Lrke.local>
2024-11-07 09:15:30 +0100

    user story 15

--- src/ComparatorTitle.java
+++ src/ComparatorTitle.java
@@ -3,14 +3,19 @@ import java.util.Comparator;
 public class ComparatorTitle implements Comparator<Movie> {
     public int compare(Movie a, Movie b){
 
-        String[] arrOfA = a.getTitle().split(" ");
-        String [] arrOfB = b.getTitle().split(" ");
+        int result = a.getTitle().compareToIgnoreCase(b.getTitle());
+        return result;
 
-        if (arrOfA[0].compareTo(arrOfB[0]) != 0){
-            return arrOfA[0].compareTo(arrOfB[0]);
-        } else {
-            return Integer.compare(a.getYearCreated(),b.getYearCreated());
-        }
+
 
     }
 }
+/*
+String[] arrOfA = a.getTitle().split(" ");
+String [] arrOfB = b.getTitle().split(" ");
+
+        if (arrOfA[0].compareTo(arrOfB[0]) != 0){
+        return arrOfA[0].compareTo(arrOfB[0]);
+        } else {
+                return Integer.compare(a.getYearCreated(),b.getYearCreated());
+        }*/
